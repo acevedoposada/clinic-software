3aa1885adb1b97fe44fdb56a045e73cc132b34dd
David Acevedo <acevedo.posada@hotmail.com>
2024-04-30 12:21:23 -0500

    :sparkles: Add helmet

--- src/layouts/general/general.layout.tsx
+++ src/layouts/general/general.layout.tsx
@@ -16,6 +16,7 @@ import {
   CalendarLtrRegular,
   FluentIcon,
 } from '@fluentui/react-icons';
+import { Helmet } from 'react-helmet';
 import { useStyles } from './general.styles';
 import { useUser } from 'hooks/user.hook';
 import { Sidenav } from 'components/sidenav';
@@ -32,56 +33,63 @@ const GeneralLayout = () => {
   const classes = useStyles();
 
   return (
-    <main className={classes.layout}>
-      <div className={mergeClasses(classes.container, 'flex')}>
-        <div className='h-full hidden md:flex'>
-          <Sidenav />
-        </div>
-        <div className={mergeClasses(classes.content, 'flex-auto')}>
-          <nav className={mergeClasses(classes.navbar, 'w-full max-w-[100vw]')}>
-            <aside className='h-full flex items-center gap-3 w-1/2'>
-              <Avatar
-                size={36}
-                color='cornflower'
-                icon={<NavbarIcon fontSize={24} className='opacity-70' />}
-              />
-              <h1 className={mergeClasses(classes.title, 'truncate')}>
-                {title}
-              </h1>
-            </aside>
-            <aside className='h-full flex'>
-              <Menu>
-                <MenuTrigger disableButtonEnhancement>
-                  <Button appearance='transparent' className='h-full w-44'>
-                    <Persona
-                      textPosition='before'
-                      name={user.name as string}
-                      presence={{ status: 'available' }}
-                      secondaryText={user.specialization as string}
-                      avatar={{ color: 'colorful' }}
-                    />
-                  </Button>
-                </MenuTrigger>
-                <MenuPopover>
-                  <MenuList>
-                    <MenuItem icon={<PersonRegular />} disabled>
-                      Perfil
-                    </MenuItem>
-                    <MenuItem icon={<ArrowExitFilled />}>
-                      Cerrar sesión
-                    </MenuItem>
-                  </MenuList>
-                </MenuPopover>
-              </Menu>
-            </aside>
-          </nav>
-          <div className='relative overflow-x-hidden h-full'>
-            <Outlet />
+    <>
+      <Helmet>
+        <title>Gestión Hospitalaria - {title}</title>
+      </Helmet>
+      <main className={classes.layout}>
+        <div className={mergeClasses(classes.container, 'flex')}>
+          <div className='h-full hidden md:flex'>
+            <Sidenav />
+          </div>
+          <div className={mergeClasses(classes.content, 'flex-auto')}>
+            <nav
+              className={mergeClasses(classes.navbar, 'w-full max-w-[100vw]')}
+            >
+              <aside className='h-full flex items-center gap-3 w-1/2'>
+                <Avatar
+                  size={36}
+                  color='cornflower'
+                  icon={<NavbarIcon fontSize={24} className='opacity-70' />}
+                />
+                <h1 className={mergeClasses(classes.title, 'truncate')}>
+                  {title}
+                </h1>
+              </aside>
+              <aside className='h-full flex'>
+                <Menu>
+                  <MenuTrigger disableButtonEnhancement>
+                    <Button appearance='transparent' className='h-full w-44'>
+                      <Persona
+                        textPosition='before'
+                        name={user.name as string}
+                        presence={{ status: 'available' }}
+                        secondaryText={user.specialization as string}
+                        avatar={{ color: 'colorful' }}
+                      />
+                    </Button>
+                  </MenuTrigger>
+                  <MenuPopover>
+                    <MenuList>
+                      <MenuItem icon={<PersonRegular />} disabled>
+                        Perfil
+                      </MenuItem>
+                      <MenuItem icon={<ArrowExitFilled />}>
+                        Cerrar sesión
+                      </MenuItem>
+                    </MenuList>
+                  </MenuPopover>
+                </Menu>
+              </aside>
+            </nav>
+            <div className='relative overflow-x-hidden h-full'>
+              <Outlet />
+            </div>
           </div>
         </div>
-      </div>
-      <MobileNav />
-    </main>
+        <MobileNav />
+      </main>
+    </>
   );
 };
 
